1b0a6c6c8a99b5b632828c6a83aeda29b091efed
Kirti Sharma <kirsharm1@publicisgroupe.net>
2020-04-13 02:57:24 +0530

    unit tests

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useContext } from "react";
 import PropTypes from "prop-types";
-import NewsItem from "../newsItem/NewsItem";
+import NewsItem from "../NewsItem";
 import { GlobalContext } from "../../store/context";
 import { MORE_LABEL, LOADING_TEXT } from "../../constants";
 import {
